ab10968f0be8a19f3953632f48cfdba97d6a6507
scan3ls <scan3ls@gmail.com>
2020-08-18 22:46:08 +0000

    Fully connect peripheral nodes on delete

--- 4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -47,6 +47,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(*head);
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
+			/* fully connect nodes */
+			(*head)->prev->next = (*head)->next;
 		*head = saved_head;
 	}
 	return (1);
